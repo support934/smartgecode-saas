6caa51948fb864c7f2c3478b79116c3856dd5f72
Sumeet Vasu <sumeet@smartgeocode.io>
2025-11-26 05:09:28 -0700

    Upload correct Java Spring Boot backend with Maven wrapper for Railway

---
+++ src/test/java/com/smartgeocode/SmartgeocodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartgeocode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartgeocodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

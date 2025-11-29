f3d2ea6511fc1d5e806ea75258f735bc19fb1efe
Sumeet Vasu <sumeet@smartgeocode.io>
2025-11-29 06:47:40 -0700

    Overwrite remote with local GeocodeController and SmartgecodeApplication for OSM endpoint

---
+++ src/main/java/io/smartgeocode/SmartGeoCodeController.java
@@ -0,0 +1,13 @@
+package io.smartgecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "io.smartgecode")  // Scans controller package for endpoints
+public class SmartgecodeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartgecodeApplication.class, args);
+    }
+}
